37a235dc54fcc38e2cd89163a404cade684149d1
Shunichiro Ikeno <ikeno-shunichiro730@g.ecc.u-tokyo.ac.jp>
2020-10-12 00:48:52 +0900

    floatsim 繰り上げのない加算で誤差が既定と同じになるようにした

--- fsim/floatsim.c
+++ fsim/floatsim.c
@@ -69,12 +69,32 @@ void CatSEF(sef *b){    //各部位をマスクしていることに注意
   b->raw = utof(b->s + b->e + b->f);
 }
 
+unsigned int RN(unsigned int f28){ // X Y a_-1 a_-2 ... a_-23 a_-24 a_-25 a_-26 の下3bitを丸める
+  unsigned int ulp = (f28 >> 3) & 1;
+  unsigned int u2 = (f28 >> 2) & 1;
+  unsigned int u4 = (f28 >> 1) & 1;
+  unsigned int u8 = f28 & 1;
+  unsigned int ret;
+  ret = (f28 >> 4) + (ulp & u2);
+  ret = (ret << 1) + ((ulp ^ u2) & (ulp | u4 | u8));
+  return ret;
+}
+
+unsigned int AddFrac(unsigned int lf, unsigned int sf, unsigned int shift){
+  unsigned int mask = (1 << (shift - 2)) - 1;
+  unsigned int u8 = ((sf & mask) > 0);    // 1/8ulp以下をORしている
+  unsigned int lf28 = ((1 << 23) + lf) << 3;
+  unsigned int sf28 = ((((1 << 23) + sf) >> (shift - 2)) << 1) + u8;
+  return RN(lf28 + sf28);
+}
+
 int main(){
   sef a, b, ans;
   sef *l, *s;
   while(scanf("%f %f", &a.raw, &b.raw) != 0){
     PrintFloatBin(a.raw);
     PrintFloatBin(b.raw);
+    printf("%f\n", a.raw + b.raw);
     PrintFloatBin(a.raw + b.raw);
     SepSEF(&a);
     SepSEF(&b);
@@ -88,7 +108,7 @@ int main(){
     }
     ans.s = l->s;
     ans.e = l->e;
-    ans.f = l->f + ((s->f) >> ((l->e - s->e) >> 23));
+    ans.f = AddFrac(l->f, s->f, ((l->e - s->e) >> 23));
     CatSEF(&ans);
     printf("%f\n", ans.raw);
     PrintFloatBin(ans.raw);

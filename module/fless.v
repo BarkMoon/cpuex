4076f94a1f77cb8486707b7f08b7538ef8af4949
Shunichiro Ikeno <ikeno-shunichiro730@g.ecc.u-tokyo.ac.jp>
2020-11-24 15:13:45 +0900

    悲しみのftoiと、めげずに頑張ったfeq, fless

--- module/fless.v
+++ module/fless.v
@@ -11,10 +11,11 @@ wire [30:0] abs1 = x1[30:0];
 wire s2 = x2[31];
 wire [30:0] abs2 = x2[30:0];
 
-wire absless;
+wire abslt, abseq;
 
-assign absless = (abs1 < abs2);
-assign y = ((s1 & ~s2) || ((~s1 & ~s2) & absless) || ((s1 & s2) & ~absless));
+assign abslt = (abs1 < abs2);
+assign abseq = (abs1 == abs2);
+assign y = ((~abseq) & ((s1 & ~s2) || ((~s1 & ~s2) & abslt) || ((s1 & s2) & ~abslt)));
 
 endmodule
-`default_nettype wire:
+`default_nettype wire
